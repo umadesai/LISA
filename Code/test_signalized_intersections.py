1937fed20d35dae2100bbc1c7d1c601e2d29f88b
Uma Desai <uma.desai@students.olin.edu>
2019-02-11 20:15:03 -0500

    add pytest.raises example

--- Code/test_signalized_intersections.py
+++ Code/test_signalized_intersections.py
@@ -2,23 +2,29 @@ from Signalized_Intersections import load_osm_graph, update_graph, \
     get_signal_coords, trunc, get_pink_node_coords
 import pytest
 
+
+def test_trunc():
+    # pytest uses a plain assert statement
+    assert trunc((70.001201, 38.9011924)) == (70.0012, 38.9012)
+
+
+@pytest.mark.parametrize("long, lat", [
+    ((70, 39)),
+    ((70, 31.2)),
+    ((70.1, 39)),
+])
 # the @pytest.mark.parametrize decorator allows us to test different
 # combinations of values in one function
-
-
-@pytest.mark.parametrize("long, lat, expected_long, expected_lat", [
-    ((70.001201, 38.9011924, 70.0012, 38.9012)),
-    ((69.899980, -39.0131429, 69.9000, -39.0131)),
-    ((70, 39, 70, 39))
-])
-def test_trunc(long, lat, expected_long, expected_lat):
-    assert trunc((long, lat)) == (expected_long, expected_lat)
-
-# the @pytest.fixture decorator helps us avoid repition when we set up helper
-# code like loading an osmnx graph or initializing a class
+def test_trunc_exception(long, lat):
+    # pytest.raises allows us to test exceptions
+    with pytest.raises(TypeError) as e:
+        trunc((long, lat))
+    assert str(e.value) == 'Please provide a tuple of floats'
 
 
 @pytest.fixture
+# the @pytest.fixture decorator helps us avoid repition when we set up
+# helper code like loading an osmnx graph or initializing a class
 def G():
     '''
     Returns osmnx graph of DC
